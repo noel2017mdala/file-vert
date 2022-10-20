b47cb00c575f32dd599469a481a6bf33863765bf
noel2017mdala <noelmdala2017@gmail.com>
2022-10-20 22:32:11 +0200

    added user sign up

--- client_app/src/components/Pages/Login.js
+++ client_app/src/components/Pages/Login.js
@@ -3,8 +3,54 @@ import emailValidator from "../../helper/emailValidator";
 import { Link } from "react-router-dom";
 import file from "../../images/file.png";
 import google from "../../images/google.svg";
+import SignUp from "./Signup";
 
 const Login = () => {
+  const [tabState, changeStabState] = useState({
+    login: true,
+    createAccount: false,
+  });
+  return (
+    <div>
+      {tabState.login ? (
+        <UserLogin />
+      ) : tabState.createAccount ? (
+        <SignUp />
+      ) : null}
+      {tabState.login ? (
+        <button
+          className="w-max p-3 -ml-3"
+          onClick={() => {
+            changeStabState({
+              login: false,
+              createAccount: true,
+            });
+          }}
+        >
+          <span className="text-sm tracking-wide text-blue-600">
+            Create new account
+          </span>
+        </button>
+      ) : tabState.createAccount ? (
+        <button
+          className="w-max p-3 -ml-3"
+          onClick={() => {
+            changeStabState({
+              login: true,
+              createAccount: false,
+            });
+          }}
+        >
+          <span className="text-sm tracking-wide text-blue-600">
+            Already have an account
+          </span>
+        </button>
+      ) : null}
+    </div>
+  );
+};
+
+const UserLogin = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
@@ -14,71 +60,65 @@ const Login = () => {
     emailErrMsg: "",
     passwordErrMsg: "",
   });
-
-  const [tabState, changeStabState] = useState({
-    login: true,
-    createAccount: false,
-  });
   return (
-    <div>
-      <div className="">
-        <div className="flex flex-col space-y-4">
-          <div className="flex space-x-4">
-            <img src={file} alt="file" className="w-8 h-8" />
-            {/* <p className="text-2xl hover:text-darkGrayishBlue">File-vert</p> */}
-            <Link
-              to="/ge-started"
-              className="text-2xl hover:text-darkGrayishBlue"
-            >
-              File-vert
-            </Link>
+    <div className="">
+      <div className="flex flex-col space-y-4">
+        <div className="flex space-x-4">
+          <img src={file} alt="file" className="w-8 h-8" />
+          {/* <p className="text-2xl hover:text-darkGrayishBlue">File-vert</p> */}
+          <Link
+            to="/ge-started"
+            className="text-2xl hover:text-darkGrayishBlue"
+          >
+            File-vert
+          </Link>
+        </div>
+
+        <p>Welcome to File-vert! Login first</p>
+      </div>
+
+      <div className="mt-12 grid gap-6 sm:grid-cols-2">
+        <button className="py-3 px-6 rounded-xl bg-blue-50 hover:bg-blue-100 focus:bg-blue-100 active:bg-blue-200">
+          <div className="flex gap-4 justify-center">
+            <img src={google} className="w-5" alt="" />
+            <span className="block w-max font-medium tracking-wide text-sm text-blue-700">
+              with Google
+            </span>
           </div>
+        </button>
+        <button className="py-3 px-6 rounded-xl bg-gray-900 transition hover:bg-gray-800 active:bg-gray-600 focus:bg-gray-700">
+          <div className="flex gap-4 items-center justify-center text-white">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              className="w-5"
+              viewBox="0 0 16 16"
+            >
+              <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+            </svg>
+            <span className="block w-max font-medium tracking-wide text-sm text-white">
+              with Github
+            </span>
+          </div>
+        </button>
+      </div>
 
-          <p>Welcome to File-vert! Login first</p>
-        </div>
+      <div role="hidden" className="mt-12 border-t">
+        <span className="block w-max mx-auto -mt-3 px-4 text-center text-gray-500 bg-white">
+          Or
+        </span>
+      </div>
 
-        <div className="mt-12 grid gap-6 sm:grid-cols-2">
-          <button className="py-3 px-6 rounded-xl bg-blue-50 hover:bg-blue-100 focus:bg-blue-100 active:bg-blue-200">
-            <div className="flex gap-4 justify-center">
-              <img src={google} className="w-5" alt="" />
-              <span className="block w-max font-medium tracking-wide text-sm text-blue-700">
-                with Google
-              </span>
-            </div>
-          </button>
-          <button className="py-3 px-6 rounded-xl bg-gray-900 transition hover:bg-gray-800 active:bg-gray-600 focus:bg-gray-700">
-            <div className="flex gap-4 items-center justify-center text-white">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                className="w-5"
-                viewBox="0 0 16 16"
-              >
-                <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
-              </svg>
-              <span className="block w-max font-medium tracking-wide text-sm text-white">
-                with Github
-              </span>
-            </div>
-          </button>
-        </div>
-
-        <div role="hidden" className="mt-12 border-t">
-          <span className="block w-max mx-auto -mt-3 px-4 text-center text-gray-500 bg-white">
-            Or
-          </span>
-        </div>
-
-        <form className="space-y-6 py-6">
-          <div>
-            <label className="block mb-2 text-sm font-medium text-brightRedLight">
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              value={email}
-              className="
+      <form className="space-y-6 py-6">
+        <div>
+          {/* <h2 className="text-2xl font-bold">Login</h2> */}
+          <label className="block mb-2 text-sm font-medium text-brightRedLight">
+            Email
+          </label>
+          <input
+            type="email"
+            value={email}
+            className="
               shadow
             appearance-none
             border
@@ -90,34 +130,34 @@ const Login = () => {
             leading-tight
             focus:outline-none focus:shadow-outline
               "
-              placeholder="name@filevert.com"
-              required
-              onChange={(text) => {
-                setErrorState({
-                  emailError: false,
-                  passwordErr: false,
-                  emailErrMsg: "",
-                  passwordErrMsg: "",
-                });
-                setEmail(text.target.value);
-              }}
-            />
-          </div>
+            placeholder="name@filevert.com"
+            required
+            onChange={(text) => {
+              setErrorState({
+                emailError: false,
+                passwordErr: false,
+                emailErrMsg: "",
+                passwordErrMsg: "",
+              });
+              setEmail(text.target.value);
+            }}
+          />
+        </div>
 
-          <p className="text-red-500 text-sm italic py-2">
-            {errorState.emailError ? errorState.emailErrMsg : null}
-          </p>
+        <p className="text-red-500 text-sm italic py-2">
+          {errorState.emailError ? errorState.emailErrMsg : null}
+        </p>
 
-          <div>
-            <label className="block mb-2 text-sm font-medium text-brightRedLight">
-              Password
-            </label>
-            <div className="flex flex-col items-end">
-              <input
-                type="password"
-                id="password"
-                value={password}
-                className="shadow
+        <div>
+          <label className="block mb-2 text-sm font-medium text-brightRedLight">
+            Password
+          </label>
+          <div className="flex flex-col items-end">
+            <input
+              type="password"
+              id="password"
+              value={password}
+              className="shadow
             appearance-none
             border
             rounded
@@ -127,81 +167,75 @@ const Login = () => {
             text-gray-500
             leading-tight
             focus:outline-none focus:shadow-outline"
-                placeholder="**********"
-                required
-                onChange={(text) => {
-                  setErrorState({
-                    emailError: false,
-                    passwordErr: false,
-                    emailErrMsg: "",
-                    passwordErrMsg: "",
-                  });
-                  setPassword(text.target.value);
-                }}
-              />
-              <button type="reset" className="w-max p-3 -mr-3">
-                <span className="text-sm tracking-wide text-blue-600">
-                  Forgot password ?
-                </span>
-              </button>
-            </div>
-
-            <p className="text-red-500 text-sm italic py-2">
-              {errorState.passwordErr ? errorState.passwordErrMsg : null}
-            </p>
-          </div>
-
-          <div>
-            <button
-              className="w-full px-6 py-3 rounded-xl bg-brightRed  transition hover:bg-brightRedLight focus:bg-brightRedLight active:bg-brightRed"
-              onClick={(e) => {
-                e.preventDefault();
-                // console.log({ email, password });
-
-                if (email === "" && password === "") {
-                  setErrorState({
-                    emailError: true,
-                    passwordErr: true,
-                    emailErrMsg: "Please enter your email",
-                    passwordErrMsg: "Please enter your password",
-                  });
-                } else if (email === "") {
-                  console.log("here");
-                  setErrorState({
-                    emailError: true,
-                    emailErrMsg: "Please enter your email",
-                  });
-                } else if (!emailValidator(email)) {
-                  setErrorState({
-                    emailError: true,
-                    emailErrMsg: "Please enter a valid email address",
-                  });
-                } else if (password === "") {
-                  setErrorState({
-                    passwordErr: true,
-                    passwordErrMsg: "Please enter your password",
-                  });
-                } else if (password.length < 6) {
-                  setErrorState({
-                    passwordErr: true,
-                    passwordErrMsg:
-                      "Password should not be less than 6 characters",
-                  });
-                } else {
-                  console.log("about to login user");
-                }
+              placeholder="**********"
+              required
+              onChange={(text) => {
+                setErrorState({
+                  emailError: false,
+                  passwordErr: false,
+                  emailErrMsg: "",
+                  passwordErrMsg: "",
+                });
+                setPassword(text.target.value);
               }}
-            >
-              <span className="font-semibold text-white text-lg">Login</span>
-            </button>
-            <a href="#" type="reset" className="w-max p-3 -ml-3">
+            />
+            <button type="reset" className="w-max p-3 -mr-3">
               <span className="text-sm tracking-wide text-blue-600">
-                Create new account
+                Forgot password ?
               </span>
-            </a>
+            </button>
           </div>
-        </form>
-      </div>
+
+          <p className="text-red-500 text-sm italic py-2">
+            {errorState.passwordErr ? errorState.passwordErrMsg : null}
+          </p>
+        </div>
+
+        <div>
+          <button
+            className="w-full px-6 py-3 rounded-xl bg-brightRed  transition hover:bg-brightRedLight focus:bg-brightRedLight active:bg-brightRed"
+            onClick={(e) => {
+              e.preventDefault();
+              // console.log({ email, password });
+
+              if (email === "" && password === "") {
+                setErrorState({
+                  emailError: true,
+                  passwordErr: true,
+                  emailErrMsg: "Please enter your email",
+                  passwordErrMsg: "Please enter your password",
+                });
+              } else if (email === "") {
+                console.log("here");
+                setErrorState({
+                  emailError: true,
+                  emailErrMsg: "Please enter your email",
+                });
+              } else if (!emailValidator(email)) {
+                setErrorState({
+                  emailError: true,
+                  emailErrMsg: "Please enter a valid email address",
+                });
+              } else if (password === "") {
+                setErrorState({
+                  passwordErr: true,
+                  passwordErrMsg: "Please enter your password",
+                });
+              } else if (password.length < 6) {
+                setErrorState({
+                  passwordErr: true,
+                  passwordErrMsg:
+                    "Password should not be less than 6 characters",
+                });
+              } else {
+                console.log("about to login user");
+              }
+            }}
+          >
+            <span className="font-semibold text-white text-lg">Login</span>
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
--- client_app/src/components/Pages/Signup.js
+++ client_app/src/components/Pages/Signup.js
@@ -1,14 +1,75 @@
+import { Link } from "react-router-dom";
+import file from "../../images/file.png";
+import google from "../../images/google.svg";
 const SignUp = () => {
   return (
-    <div>
-      <form>
-        <div className="mb-6">
-          <label className="block mb-2 text-sm font-medium text-brightRedLight">
-            User Name
-          </label>
-          <input
-            type="text"
-            className="shadow
+    <div className="">
+      <div className="flex flex-col space-y-4">
+        <div className="flex space-x-4">
+          <img src={file} alt="file" className="w-8 h-8" />
+          {/* <p className="text-2xl hover:text-darkGrayishBlue">File-vert</p> */}
+          <Link
+            to="/ge-started"
+            className="text-2xl hover:text-darkGrayishBlue"
+          >
+            File-vert
+          </Link>
+        </div>
+
+        <p>Welcome to File-vert! Please create your account</p>
+      </div>
+
+      <div className="mt-12 grid gap-6 sm:grid-cols-2">
+        <button className="py-3 px-6 rounded-xl bg-blue-50 hover:bg-blue-100 focus:bg-blue-100 active:bg-blue-200">
+          <div className="flex gap-4 justify-center">
+            <img src={google} className="w-5" alt="" />
+            <span className="block w-max font-medium tracking-wide text-sm text-blue-700">
+              with Google
+            </span>
+          </div>
+        </button>
+        <button className="py-3 px-6 rounded-xl bg-gray-900 transition hover:bg-gray-800 active:bg-gray-600 focus:bg-gray-700">
+          <div className="flex gap-4 items-center justify-center text-white">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              className="w-5"
+              viewBox="0 0 16 16"
+            >
+              <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+            </svg>
+            <span className="block w-max font-medium tracking-wide text-sm text-white">
+              with Github
+            </span>
+          </div>
+        </button>
+      </div>
+
+      <div role="hidden" className="mt-12 border-t">
+        <span className="block w-max mx-auto -mt-3 px-4 text-center text-gray-500 bg-white">
+          Or
+        </span>
+      </div>
+
+      <form className="space-y-6 py-6">
+        <div>
+          <h2 className="text-2xl font-bold">Sign Up</h2>
+          <p className="text-sm my-2">
+            Create your account. It's free and
+            <span className="text-brightRedLight"> No credit card needed</span>
+          </p>
+        </div>
+
+        <div className="flex flex-col space-y-6 md:flex-row md:space-x-6 md:space-y-0">
+          <div>
+            <label className="block mb-2 text-sm font-medium text-brightRedLight">
+              First Name
+            </label>
+            <input
+              type="text"
+              // value={email}
+              className="
+              shadow
             appearance-none
             border
             rounded
@@ -17,19 +78,48 @@ const SignUp = () => {
             px-3
             text-gray-500
             leading-tight
-            focus:outline-none focus:shadow-outline"
-            placeholder="User Name"
-            required
-          />
+            focus:outline-none focus:shadow-outline
+              "
+              placeholder="John"
+              required
+            />
+          </div>
+
+          <div>
+            <label className="block mb-2 text-sm font-medium text-brightRedLight">
+              Last Name
+            </label>
+            <input
+              type="text"
+              // value={email}
+              className="
+              shadow
+            appearance-none
+            border
+            rounded
+            w-full
+            py-3
+            px-3
+            text-gray-500
+            leading-tight
+            focus:outline-none focus:shadow-outline
+              "
+              placeholder="Doe"
+              required
+            />
+          </div>
         </div>
 
-        <div className="mb-6">
+        <div>
+          {/* <h2 className="text-2xl font-bold">Login</h2> */}
           <label className="block mb-2 text-sm font-medium text-brightRedLight">
             Email
           </label>
           <input
             type="email"
-            className="shadow
+            // value={email}
+            className="
+              shadow
             appearance-none
             border
             rounded
@@ -38,19 +128,23 @@ const SignUp = () => {
             px-3
             text-gray-500
             leading-tight
-            focus:outline-none focus:shadow-outline"
+            focus:outline-none focus:shadow-outline
+              "
             placeholder="name@filevert.com"
             required
           />
         </div>
 
-        <div className="mb-6">
+        <div>
+          {/* <h2 className="text-2xl font-bold">Login</h2> */}
           <label className="block mb-2 text-sm font-medium text-brightRedLight">
             Phone Number
           </label>
           <input
-            type="number"
-            className="shadow
+            type="text"
+            // value={email}
+            className="
+              shadow
             appearance-none
             border
             rounded
@@ -59,19 +153,22 @@ const SignUp = () => {
             px-3
             text-gray-500
             leading-tight
-            focus:outline-none focus:shadow-outline"
+            focus:outline-none focus:shadow-outline
+              "
             placeholder="Phone Number"
             required
           />
         </div>
 
-        <div className="mb-6">
+        <div>
           <label className="block mb-2 text-sm font-medium text-brightRedLight">
             Password
           </label>
-          <input
-            type="password"
-            className="shadow
+          <div className="flex flex-col items-end">
+            <input
+              type="password"
+              // value={password}
+              className="shadow
             appearance-none
             border
             rounded
@@ -81,21 +178,34 @@ const SignUp = () => {
             text-gray-500
             leading-tight
             focus:outline-none focus:shadow-outline"
-            placeholder="**********"
-            required
-          />
+              placeholder="**********"
+              required
+            />
+          </div>
         </div>
 
-        <button
-          type="submit"
-          className="bg-brightRedLight text-white   font-medium rounded-lg text-sm w-full  px-5 py-2.5 text-center hover:bg-brightRed"
-          onClick={(e) => {
-            e.preventDefault();
-            console.log("you are about to login");
-          }}
-        >
-          Login
-        </button>
+        <div className="flex items-start mb-6">
+          <div className="flex items-center h-5">
+            <input
+              id="remember"
+              type="checkbox"
+              value=""
+              className="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
+              required
+            />
+          </div>
+          <label className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+            I have accepted the{" "}
+            <span className="text-brightRedLight underline"> Terms of use</span> and
+            <span className="text-brightRedLight underline"> privacy policy</span>
+          </label>
+        </div>
+
+        <div>
+          <button className="w-full px-6 py-3 rounded-xl bg-brightRed  transition hover:bg-brightRedLight focus:bg-brightRedLight active:bg-brightRed">
+            <span className="font-semibold text-white text-lg">Sign Up</span>
+          </button>
+        </div>
       </form>
     </div>
   );
