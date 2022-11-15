47020db82a71c1472c797674ec1d0e29a73bf276
noel2017mdala <noelmdala2017@gmail.com>
2022-11-15 12:40:39 +0200

    finish plan list

--- client_app/src/components/settings/Billing.js
+++ client_app/src/components/settings/Billing.js
@@ -31,56 +31,60 @@ const Billing = () => {
           </div>
         </div>
 
-        <div className="flex flex-col space-y-8 md:flex-row space-x-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-12 lg:gap-8 py-14 md:px-8">
           {isLoading ? (
-            <p>loading</p>
+            <p>Loading....</p>
           ) : (
             data.getAllPlans.map((plan, key) => (
-              <div key={key} className="">
-                <div className="p-4 w-full max-w-sm bg-white rounded-lg border shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700">
-                  <h5 className="mb-4 text-xl font-medium text-gray-500 dark:text-gray-400">
-                    {plan.name} plan
-                  </h5>
-                  <div className="flex items-baseline text-gray-900 dark:text-white">
-                    <span className="text-3xl font-semibold">$</span>
-                    <span className="text-5xl font-extrabold tracking-tight">
-                      {plan.price}
-                    </span>
-                    <span className="ml-1 text-xl font-normal text-gray-500 dark:text-gray-400">
-                      /month
-                    </span>
-                  </div>
+              <div
+                className="border border-gray-200 shadow-lg p-8 bg-white rounded-xl relative flex flex-col"
+                key={key}
+              >
+                <h5 className="mb-4 text-xl text-gray-500 font-semibold leading-5">
+                  {plan.name} plan
+                </h5>
 
-                  <ul role="list" className="my-7 space-y-5">
-                    {plan.features.map((features, i) => (
-                      <li className="flex space-x-3" key={i}>
-                        <svg
-                          aria-hidden="true"
-                          className="flex-shrink-0 w-5 h-5 text-blue-600 dark:text-blue-500"
-                          fill="currentColor"
-                          viewBox="0 0 20 20"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <title>Check icon</title>
-                          <path
-                            fillRule="evenodd"
-                            d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                            clipRule="evenodd"
-                          ></path>
-                        </svg>
-                        <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">
-                          {plan.features[i]}
-                        </span>
-                      </li>
-                    ))}
-                  </ul>
-                  <button
-                    type="button"
-                    className="text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center"
-                  >
-                    Choose plan
-                  </button>
+                {plan.name === "professional" && <p className="absolute top-0 -translate-y-1/2 bg-brightRed text-white px-3 py-0.5 rounded-full text-sm font-semibold tracking-wide shadow-md">Most Popular</p>}
+                <div className="flex items-baseline space-x-2 text-gray-900 bg-gray-50">
+                  <span className="text-3xl font-semibold">$</span>
+                  <span className="text-4xl font-extrabold tracking-tight">
+                    {plan.price}
+                  </span>
+                  <span className="ml-1 text-xl font-normal text-gray-500 ">
+                    /month
+                  </span>
                 </div>
+
+                <ul role="list" className="my-7 space-y-5 flex-1">
+                      {plan.features.map((features, i) => (
+                        <li className="flex space-x-3" key={i}>
+                          <svg
+                            aria-hidden="true"
+                            className="flex-shrink-0 w-5 h-5 text-brightRedLight "
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <title>Check icon</title>
+                            <path
+                              fillRule="evenodd"
+                              d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                              clipRule="evenodd"
+                            ></path>
+                          </svg>
+                          <span className="text-base font-normal leading-tight text-gray-500">
+                            {plan.features[i]}
+                          </span>
+                        </li>
+                      ))}
+                    </ul>
+
+                    <button
+                      type="button"
+                      className="text-white bg-brightRed hover:bg-brightRedLight focus:ring-4 focus:outline-none focus:ring-blue-200 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center shadow-md"
+                    >
+                      Choose plan
+                    </button>
               </div>
             ))
           )}
